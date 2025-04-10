13d18678eb9ea089894b3d89ca9bc2cb96b329f6
Include caleb.fun <oasis.mystre@gmail.com>
2025-04-10 03:25:33 +0100

    ...

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -2,9 +2,10 @@ import { message } from "telegraf/filters";
 import type { Telegraf } from "telegraf";
 
 import { onStart } from "./onStart";
+import { protectedCommand } from "../utils";
 import { onNewChatMembers } from "./onNewChatMembers";
 
 export const registerCommands = (botInstance: Telegraf) => {
-  botInstance.start(onStart);
+  botInstance.start(protectedCommand(onStart, "private"));
   botInstance.on(message("new_chat_members"), onNewChatMembers);
 };
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,1 +1,13 @@
+import { Context } from "telegraf";
+
 export * from "./format";
+
+export const protectedCommand =
+  <T extends (context: Context) => void>(
+    fn: T,
+    ...rooms: ("private" | "channel" | "group" | "supergroup")[]
+  ) =>
+  (context: Context) => {
+    if (context.chat && rooms.includes(context.chat.type)) return fn(context);
+    return;
+  };
